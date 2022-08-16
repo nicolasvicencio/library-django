cd2395b234dd9fdcec699a12593a403b3026e8fa
nicolas-manjaro <nicolas.vicencio.or@protonmail.com>
2022-08-15 23:16:19 -0400

    Starting project with django l

---
+++ crud/library/urls.py
@@ -0,0 +1,10 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+		path('', views.home, name='home'),
+		path('about', views.about, name='about'),
+		path('books', views.books, name='books'),
+		path('books/create', views.createBook, name='create')
+]
+
---
+++ crud/library/views.py
@@ -0,0 +1,14 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+
+# Create your views here.
+def home(request):
+	return render(request, 'pages/home.html')
+def about(request): 
+	return render(request, 'pages/about.html')
+
+
+def books(request):
+	return render(request, 'books/index.html')
+def createBook(request):
+	return render(request, 'books/create.html')
